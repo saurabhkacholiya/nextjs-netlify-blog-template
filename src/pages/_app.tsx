1007915e70e15dc6af4e4c81cc7eee0510bbf72a
Vishwanath Arondekar <vishwanath@Vishwanathas-MacBook-Pro.local>
2021-06-13 17:46:56 +0530

    feat: tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "normalize.css";
+import 'tailwindcss/tailwind.css'
 import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
-import "../../public/styles/global.css";
+// import "../../public/styles/global.css";
 
 if(typeof window !== "undefined"){
   window.CMS_MANUAL_INIT = true;
